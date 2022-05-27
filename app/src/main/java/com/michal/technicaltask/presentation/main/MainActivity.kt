32fc2848507b1ab196ebaf6ceec4d5cc77b0f404
Michał Dobrzański <mdobrzanski@westwing.pl>
2022-05-27 11:54:35 +0200

    adjusted fragment architecture with ui

---
+++ app/src/main/java/com/michal/technicaltask/presentation/main/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.michal.technicaltask.presentation.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.michal.technicaltask.R
+import com.michal.technicaltask.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+}
